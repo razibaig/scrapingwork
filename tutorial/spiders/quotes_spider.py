3680294bdd3120289587877d58cae017e32a2d1f
razibaig <raziullah.baig@arbisoft.com>
2018-07-17 19:00:13 +0500

    parsing into json

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -2,8 +2,14 @@ import scrapy
 from bs4 import BeautifulSoup as B
 from parsel import Selector
 import copy
+import json
+from json import dumps, loads, JSONEncoder, JSONDecoder
+import pickle
+import sys
+
 
 DATA_LIST = {}
+sys.setrecursionlimit(10000)
 
 
 class QuotesSpider(scrapy.Spider):
@@ -48,20 +54,18 @@ class QuotesSpider(scrapy.Spider):
             table_list = copy.deepcopy(self.print_table(table, "engineTable"))
             data_dict[ind] = table_list
             ind += 1
-        print(data_dict)
+        # print(data_dict)
 
         player_table_s = response.css("div#shrtPrfl").extract_first()
         if player_table_s:
             player_prof_s = B(''.join(player_table_s), 'html.parser')
             for tag in player_prof_s.find_all('p'):
-                # print(tag.string)
                 player.profile_info.append(tag.string)
 
         player_table = response.css("div#plrpfl").extract()
         if player_table:
             player_prof = B(''.join(player_table), 'html.parser')
             for tag in player_prof.find_all('p'):
-                # print(tag.string)
                 player.profile_info.append(tag.string)
 
         # print(player.profile_info)
@@ -69,16 +73,27 @@ class QuotesSpider(scrapy.Spider):
         articles = response.css("div.headline").extract_first()
         soup = B(''.join(articles), 'html.parser')
         for a in soup.find_all('a', href=True):
-            # print('{0}{1}'.format("http://www.espncricinfo.com", a['href']))
             player.latest_articles.append("http://www.espncricinfo.com" + a['href'])
 
         photos = response.css("div.ciPicHldr").extract()
         soup = B(''.join(photos), 'html.parser')
         for a in soup.find_all('a', href=True):
-            # print('{0}'.format(soup.img['src']))
             player.latest_photos.append(soup.img['src'])
 
-        player.print_player()
+        # player.print_player()
+
+        # json_string = json.dumps(player.__dict__)
+        # json_string = json.dumps(player, default=set_default)
+
+        json_string = dumps(player, cls=PythonObjectEncoder)
+        print(json_string)
+
+        print(loads(json_string, object_hook=as_python_object))
+
+        filename = 'player.json'
+        file = open(filename, "w")
+        file.write(json_string)
+        file.close()
 
     def print_table(self, table, class_name):
         soup = B(''.join(table), "html.parser")
@@ -98,6 +113,40 @@ class QuotesSpider(scrapy.Spider):
                 # print("{0}{1}{2}".format(field[0].strip(), "\t", field[1].strip()))
 
         return DATA_LIST
+
+
+def obj_dict(obj):
+    return obj.__dict__
+
+
+def obj_str(obj):
+    return obj.__str__
+
+
+def set_default(Player):
+    if isinstance(Player, set):
+        return list(Player)
+    raise TypeError
+
+
+class SetEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (list, dict, str, int, float, bool, type(None))):
+            return json.JSONEncoder.default(self, obj)
+        return json.JSONEncoder.default(self, obj)
+
+
+class PythonObjectEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (list, dict, str, int, float, bool, type(None))):
+            return JSONEncoder.default(self, obj)
+        return {'_python_object': pickle.dumps(obj)}
+
+
+def as_python_object(dct):
+    if '_python_object' in dct:
+        return pickle.loads(str(dct['_python_object']))
+    return dct
 
 
 class Player:
@@ -136,3 +185,9 @@ class Player:
         print(self.profile_info)
         print(self.latest_articles)
         print(self.latest_photos)
+
+    def __dict__(self):
+        return self.__dict__
+
+    def __str__(self):
+        return self.__str__
