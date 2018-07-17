bd5218d0d67ebfb80c9ed4c5658f04139d42763a
Raziullah Baig <38754771+razibaig@users.noreply.github.com>
2018-07-18 03:58:59 +0500

    json render added

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -3,13 +3,6 @@ from bs4 import BeautifulSoup as B
 from parsel import Selector
 import copy
 import json
-from json import dumps, loads, JSONEncoder, JSONDecoder
-import pickle
-import sys
-
-
-DATA_LIST = {}
-sys.setrecursionlimit(10000)
 
 
 class QuotesSpider(scrapy.Spider):
@@ -17,14 +10,16 @@ class QuotesSpider(scrapy.Spider):
     start_urls = [
         'http://www.espncricinfo.com/england/content/player/351588.html',
     ]
+    INDEX = 0
+    DATA_LIST = {}
 
     def parse(self, response):
         player = Player()
-        data_dict = {}
+        p_info = []
+        l_articles = []
+        l_photos = []
 
-        page = response.url.split("/")[-2]
-        filename = '%s.html' % page
-        file = open(filename, "w")
+        file = open("player.html", "w")
         file.write(response.text)
         file.close()
 
@@ -38,7 +33,6 @@ class QuotesSpider(scrapy.Spider):
         for tag in head:
             t = head.__getitem__(i)
             key = t.contents[0].string
-            # print(key)
             info = []
             for j in range(2, len(t.contents)):
                 if t.contents[j].string != ' ':
@@ -46,56 +40,70 @@ class QuotesSpider(scrapy.Spider):
             personal_info[key] = info
             i += 1
 
-        player.add_person_info(personal_info)
+        player.personal_info['Personal Information'] = personal_info
 
         ind = 0
         all_tables = response.css("table.engineTable").extract()
         for table in all_tables:
-            table_list = copy.deepcopy(self.print_table(table, "engineTable"))
-            data_dict[ind] = table_list
+            (self.print_table(table, "engineTable"))
             ind += 1
-        # print(data_dict)
+        # print(self.DATA_LIST)
+
+        player.batting_averages['Batting Averages'] = self.DATA_LIST[0]
+        player.bowling_averages['Bowling Averages'] = self.DATA_LIST[1]
+        player.recent_matches['Recent Mathces'] = self.DATA_LIST[3]
 
         player_table_s = response.css("div#shrtPrfl").extract_first()
         if player_table_s:
             player_prof_s = B(''.join(player_table_s), 'html.parser')
             for tag in player_prof_s.find_all('p'):
-                player.profile_info.append(tag.string)
+                p_info.append(tag.string)
 
         player_table = response.css("div#plrpfl").extract()
         if player_table:
             player_prof = B(''.join(player_table), 'html.parser')
             for tag in player_prof.find_all('p'):
-                player.profile_info.append(tag.string)
+                p_info.append(tag.string)
 
+        player.profile_info['Profile Info'] = p_info
         # print(player.profile_info)
 
         articles = response.css("div.headline").extract_first()
         soup = B(''.join(articles), 'html.parser')
         for a in soup.find_all('a', href=True):
-            player.latest_articles.append("http://www.espncricinfo.com" + a['href'])
+            l_articles.append("http://www.espncricinfo.com" + a['href'])
+
+        player.latest_articles['Latest Articles'] = l_articles
 
         photos = response.css("div.ciPicHldr").extract()
         soup = B(''.join(photos), 'html.parser')
         for a in soup.find_all('a', href=True):
-            player.latest_photos.append(soup.img['src'])
+            l_photos.append(soup.img['src'])
 
-        # player.print_player()
+        player.latest_photos['Latest Photos'] = l_photos
 
-        # json_string = json.dumps(player.__dict__)
-        # json_string = json.dumps(player, default=set_default)
+        json_write = []
 
-        json_string = dumps(player, cls=PythonObjectEncoder)
-        print(json_string)
+        # json_string = json.dumps([player.personal_info, player.batting_averages, player.bowling_averages,
+        #                           player.recent_matches, player.profile_info, player.latest_articles,
+        #                           player.latest_photos])
 
-        print(loads(json_string, object_hook=as_python_object))
+        json_write.append(json.dumps([player.personal_info]))
+        json_write.append(json.dumps([player.batting_averages]))
+        json_write.append(json.dumps([player.bowling_averages]))
+        json_write.append(json.dumps([player.recent_matches]))
+        json_write.append(json.dumps([player.profile_info]))
+        json_write.append(json.dumps([player.latest_articles]))
+        json_write.append(json.dumps([player.latest_photos]))
 
-        filename = 'player.json'
-        file = open(filename, "w")
-        file.write(json_string)
-        file.close()
+        with open('player.json', "w") as file:
+            for data in json_write:
+                file.write(data + "\n")
+
+        player.print_player()
 
     def print_table(self, table, class_name):
+
         soup = B(''.join(table), "html.parser")
         table = soup.find("table", attrs={"class": class_name})
 
@@ -107,87 +115,38 @@ class QuotesSpider(scrapy.Spider):
             dataset = zip(headings, (td.get_text() for td in row.find_all("td")))
             datasets.append(dataset)
 
+        tuple_list = []
         for dataset in datasets:
+            tuple = {}
             for field in dataset:
-                DATA_LIST[field[0].strip()] = field[1].strip()
+                tuple[field[0].strip()] = field[1].strip()
                 # print("{0}{1}{2}".format(field[0].strip(), "\t", field[1].strip()))
+            tuple_list.append(tuple)
 
-        return DATA_LIST
+        self.DATA_LIST[self.INDEX] = tuple_list
+        self.INDEX += 1
 
-
-def obj_dict(obj):
-    return obj.__dict__
-
-
-def obj_str(obj):
-    return obj.__str__
-
-
-def set_default(Player):
-    if isinstance(Player, set):
-        return list(Player)
-    raise TypeError
-
-
-class SetEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, (list, dict, str, int, float, bool, type(None))):
-            return json.JSONEncoder.default(self, obj)
-        return json.JSONEncoder.default(self, obj)
-
-
-class PythonObjectEncoder(JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, (list, dict, str, int, float, bool, type(None))):
-            return JSONEncoder.default(self, obj)
-        return {'_python_object': pickle.dumps(obj)}
-
-
-def as_python_object(dct):
-    if '_python_object' in dct:
-        return pickle.loads(str(dct['_python_object']))
-    return dct
+        # print(self.DATA_LIST)
 
 
 class Player:
 
     personal_info = {}
-    fullname = []
-    born = []
-    current_age = []
-    major_teams = []
-    playing_role = []
-    batting_style = []
-    education = []
-    relation = []
-
     batting_averages = {}
     bowling_averages = {}
     recent_matches = {}
-    profile_info = []
-    latest_articles = []
-    latest_photos = []
+    profile_info = {}
+    latest_articles = {}
+    latest_photos = {}
 
     def add_person_info(self, data):
         self.personal_info = copy.deepcopy(data)
 
-    def add_bat_averages(self, data):
-        self.batting_averages = copy.deepcopy(data)
-
-    def add_bowl_averages(self, data):
-        self.bowling_averages = copy.deepcopy(data)
-
-    def add_recent_matches(self, data):
-        self.recent_matches = copy.deepcopy(data)
-
     def print_player(self):
         print(self.personal_info)
+        print(self.batting_averages)
+        print(self.bowling_averages)
+        print(self.recent_matches)
         print(self.profile_info)
         print(self.latest_articles)
         print(self.latest_photos)
-
-    def __dict__(self):
-        return self.__dict__
-
-    def __str__(self):
-        return self.__str__
